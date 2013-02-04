dcc85c683f6b6a87f6f43a259643d8a6044f45dc
Mario Signore <MSignore@ALPGHLTP069.apangeanet.com>
2013-02-04 13:00:03 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Directory::Application.config.secret_token = '748f5b4da6ae8b4a228acbccb644138ae6c202d2848d8c366619509d4c9de9cfd7981e3daffdb30fa660ea8c7b8c59508d50c43c42c52aaa11bc50b008c4b260'
