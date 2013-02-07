a115d3d50038686bc582066d0dc38bdfa562bcb7
Mario Signore <MSignore@ALPGHLTP069.apangeanet.com>
2013-02-07 14:56:43 -0500

    added default avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new({ :avatar.params[:user]
+  	@user = User.new(params[:user])
     if @user.save
       redirect_to @user
     else
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+	def avatar_for(user)
+    	avatar_file_name = "person.jpg"
+    	avatar_url = "person.jpg"
+    	image_tag(avatar_url, alt: user.name, class: "avatar")
+  	end
 end
