802e5891536011bf938bfe318d8ded81cbf47aa8
Mario Signore <mario.j.signore@gmail.com>
2013-02-12 16:56:30 -0500

    added location to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation, :avatar, :group
+  attr_accessible :name, :email, :password, :password_confirmation, :avatar, :group, :location
   has_secure_password
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
